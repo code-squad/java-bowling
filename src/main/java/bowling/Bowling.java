14142f72da2dc8a9640d3874d2e2e65933c967da
javajigi <javajigi@gmail.com>
2017-11-21 00:04:43 +0900

    tdd - implement ten frame

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -8,12 +8,14 @@ public class Bowling {
 
     public List<String> roll(int falledPin) {
         falledPins.add(falledPin);
-        processStrike(falledPin);
-        
+        if (falledPins.size() < 19) {
+            addBlankPinWhenStrike(falledPin);
+        }
+        System.out.println(falledPins);
         return createResult(falledPins);
     }
 
-    private void processStrike(int falledPin) {
+    private void addBlankPinWhenStrike(int falledPin) {
         if (isStrike(falledPin)) {
             falledPins.add(0);
         }
@@ -23,17 +25,54 @@ public class Bowling {
         List<String> result = new ArrayList<>();
         int offsetOfFrame = 0;
         while(offsetOfFrame < falledPins.size()) {
-            result.add(status(falledPins, offsetOfFrame));
-            offsetOfFrame += 2;
+            if (isTenFrame(offsetOfFrame)) {
+                result.add(createResultOf10(getTenFalledPins(falledPins, offsetOfFrame)));
+                offsetOfFrame += 3;
+            } else {
+                result.add(status(falledPins, offsetOfFrame));
+                offsetOfFrame += 2;
+            }
         }
         return result;
     }
+
+    private boolean isTenFrame(int offsetOfFrame) {
+        return offsetOfFrame == (10 - 1) * 2;
+    }
+
+    private List<Integer> getTenFalledPins(List<Integer> falledPins, int offsetOfFrame) {
+        return falledPins.subList(offsetOfFrame, falledPins.size());
+    }
     
     String createResultOf10(List<Integer> falledPinsOf10) {
-        return status(falledPinsOf10, 0);
+        return createResultOf10(falledPinsOf10, 0);
+    }
+    
+    private String createResultOf10(List<Integer> falledPinsOf10, int offsetOfFrame) {
+        if (falledPinsOf10.size() <= offsetOfFrame) {
+            return "";
+        }
+        String status = status(falledPinsOf10, offsetOfFrame);
+        if (offsetOfFrame == 2) {
+            return status;
+        }
+        
+        if (status.equals("X")) {
+            return status + createResultOf10(falledPinsOf10, offsetOfFrame + 1);
+        }
+        
+        if (falledPinsOf10.size() == 3) {
+            return status + status(falledPinsOf10, 2);
+        }
+        
+        return status(falledPinsOf10, offsetOfFrame);
     }
     
     private String status(List<Integer> falledPins, int offsetOfFrame) {
+        if (falledPins.size() <= offsetOfFrame) {
+            return "";
+        }
+        
         int first = falledPins.get(offsetOfFrame);
         if (isStrike(first)) {
             return "X";
--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -1,6 +1,6 @@
 package bowling;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -56,13 +56,57 @@ public class BowlingTest {
     }
     
     @Test
-    public void createResult_10frame() throws Exception {
-        List<Integer> falledPinsOf10 = Arrays.asList(10);
+    public void createResult_10frame_miss() throws Exception {
+        List<Integer> falledPinsOf10 = Arrays.asList(8);
         String result = bowling.createResultOf10(falledPinsOf10);
-        assertEquals("X", result);
+        assertEquals("8", result);
         
-        falledPinsOf10 = Arrays.asList(10, 10);
+        falledPinsOf10 = Arrays.asList(8, 1);
         result = bowling.createResultOf10(falledPinsOf10);
-        assertEquals("XX", result);
+        assertEquals("81", result);
+    }
+    
+    @Test
+    public void createResult_10frame_spare() throws Exception {
+        List<Integer> falledPinsOf10 = Arrays.asList(8, 2, 7);
+        String result = bowling.createResultOf10(falledPinsOf10);
+        assertEquals("8/7", result);
+    }
+    
+    @Test
+    public void createResult_10frame_strike() throws Exception {
+        List<Integer> falledPinsOf10 = Arrays.asList(10, 10, 8);
+        String result = bowling.createResultOf10(falledPinsOf10);
+        assertEquals("XX8", result);
+    }
+    
+    @Test
+    public void all_strike() throws Exception {
+        List<String> result = null;
+        for (int i = 0; i < 12; i++) {
+            result = bowling.roll(10);
+        }
+        assertEquals("XXX", result.get(9));
+    }
+    
+    @Test
+    public void tenframe_miss() throws Exception {
+        for (int i = 0; i < 9; i++) {
+            bowling.roll(10);
+        }
+        bowling.roll(8);
+        List<String> result = bowling.roll(1);
+        assertEquals("81", result.get(9));
+    }
+    
+    @Test
+    public void tenframe_spare() throws Exception {
+        for (int i = 0; i < 9; i++) {
+            bowling.roll(10);
+        }
+        bowling.roll(8);
+        bowling.roll(2);
+        List<String> result = bowling.roll(9);
+        assertEquals("8/9", result.get(9));
     }
 }
