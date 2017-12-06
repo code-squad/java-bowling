553ab4aebf6f7ea5522c81b4159eb476b6ec4e78
= <rightgear11@gmail.com>
2017-12-07 03:04:13 +0900

    make score - when first score + second score equals 10

--- src/main/java/Score.java
+++ src/main/java/Score.java
@@ -21,18 +21,29 @@ public class Score {
 	static List<String> getFirstScore(String stringScore) {
 		List<String> score = initScore();
 		if (stringScore.equals("10")) {
-			score.set(2, "X");
-			return score;
+			stringScore = "X";
+		}
+		if (stringScore.equals("0")) {
+			stringScore = "-";
 		}
 		score.set(2, stringScore);
 		return score;
 	}
 	
-	static List<String> getSecondScore(String stringFirstScore, String stringSecondScore) {
+	static List<String> getSecondScore(String firstScore, String secondScore) {
 		List<String> score = initScore();
-		score.set(2, stringFirstScore);
+		if (Integer.parseInt(firstScore) + Integer.parseInt(secondScore) == 10) {
+			secondScore = "/";
+		}
+		if (firstScore.equals("0")) {
+			firstScore = "-";
+		}
+		if (secondScore.equals("0")) {
+			secondScore = "-";
+		}
+		score.set(2, firstScore);
 		score.set(3, "|");
-		score.set(4, stringSecondScore);
+		score.set(4, secondScore);
 		return score;
 	}
 	
