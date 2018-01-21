ebe4a9a9e8a47583a25fb053caf52b783080fa1f
rogue <retrokarr@gmail.com>
2018-01-21 15:17:52 +0900

    feat : create frame status enum class

---
+++ src/main/java/bowling/enums/FrameStatus.java
@@ -0,0 +1,21 @@
+package bowling.enums;
+
+public enum FrameStatus {
+    STRIKE,
+    SPARE,
+    MISS,
+    NOT_END;
+
+    public static FrameStatus valueOf(int firstTry, int secondTry) {
+        int total = firstTry + secondTry;
+
+        if(firstTry == 10)
+            return STRIKE;
+        if(secondTry < 0)
+            return NOT_END;
+        if(total == 10)
+            return SPARE;
+        
+        return MISS;
+    }
+}
---
+++ src/test/java/bowling/enums/FrameStatusTest.java
@@ -0,0 +1,17 @@
+package bowling.enums;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class FrameStatusTest {
+    @Test
+    public void FrameStatusTest() {
+        assertThat(FrameStatus.valueOf(-1, -1)).isEqualTo(FrameStatus.NOT_END);
+        assertThat(FrameStatus.valueOf(10, -1)).isEqualTo(FrameStatus.STRIKE);
+        assertThat(FrameStatus.valueOf(8, 2)).isEqualTo(FrameStatus.SPARE);
+        assertThat(FrameStatus.valueOf(5, -1)).isEqualTo(FrameStatus.NOT_END);
+        assertThat(FrameStatus.valueOf(0, 0)).isEqualTo(FrameStatus.MISS);
+        assertThat(FrameStatus.valueOf(5, 0)).isEqualTo(FrameStatus.MISS);
+    }
+}
