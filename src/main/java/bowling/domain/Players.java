748b50330d687fd3ae181b3efda005ba83299623
rogue <retrokarr@gmail.com>
2018-01-22 19:38:03 +0900

    feat : create Players class

---
+++ src/main/java/bowling/domain/Players.java
@@ -0,0 +1,18 @@
+package bowling.domain;
+
+import java.util.List;
+
+public class Players {
+    private List<Player> players;
+
+    public Players(List<Player> players) {
+        inputValidationCheck(players);
+
+        this.players = players;
+    }
+
+    private void inputValidationCheck(List<Player> players) {
+        if(players == null)
+            throw new IllegalArgumentException("Invalid input for Players");
+    }
+}
