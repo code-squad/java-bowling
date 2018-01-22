080a89a2cc7a25ec471579a90bd09238a8333f12
rogue <retrokarr@gmail.com>
2018-01-22 14:25:14 +0900

    feat : create bowling game class

---
+++ src/main/java/bowling/domain/BowlingGame.java
@@ -0,0 +1,22 @@
+package bowling.domain;
+
+/**
+ * Created by KH on 2018-01-22.
+ */
+public class BowlingGame {
+    private String player;
+    private Frames frames;
+
+    public BowlingGame(String player) {
+        this.player = player;
+        this.frames = new Frames();
+    }
+
+    public String printView() {
+        StringBuilder sb = new StringBuilder("| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |\n");
+        sb.append("|  ").append(player).append(" |")
+                .append(frames.toString());
+
+        return sb.toString();
+    }
+}
---
+++ src/test/java/bowling/domain/BowlingGameTest.java
@@ -0,0 +1,22 @@
+package bowling.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class BowlingGameTest {
+    private BowlingGame bowlingGame;
+
+    @Before
+    public void init() {
+        bowlingGame = new BowlingGame("KKH");
+    }
+
+    @Test
+    public void showState() {
+        assertThat(bowlingGame.printView()).isEqualTo(
+                "| NAME |  01  |  02  |  03  |  04  |  05  |  06  |  07  |  08  |  09  |  10  |\n" +
+                "|  KKH |      |      |      |      |      |      |      |      |      |      |");
+    }
+}
