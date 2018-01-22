77293032823fe8633ff3a8f8f4c8e34942ad3b43
rogue <retrokarr@gmail.com>
2018-01-22 19:50:41 +0900

    feat : create PlayerQueue to express user rotation

--- src/main/java/bowling/domain/Player.java
+++ src/main/java/bowling/domain/Player.java
@@ -1,5 +1,7 @@
 package bowling.domain;
 
+import java.util.Objects;
+
 public class Player {
     private String name;
     private Frames frames;
@@ -13,5 +15,19 @@ public class Player {
     private void inputValidationCheck(String name) {
         if(name == null || name.length() != 3)
             throw new IllegalArgumentException("name must be three english letter");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name);
     }
 }
---
+++ src/main/java/bowling/domain/PlayerQueue.java
@@ -0,0 +1,21 @@
+package bowling.domain;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class PlayerQueue {
+    private Queue<Player> playerQueue;
+
+    public PlayerQueue(List<Player> players) {
+        playerQueue = new LinkedList<>(players);
+    }
+
+    public Player getCurrentPlayer() {
+        return playerQueue.peek();
+    }
+
+    public void rotate() {
+        playerQueue.add(playerQueue.poll());
+    }
+}
