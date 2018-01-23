a25091e83e08db6a75722cb323672cefb13a47c8
HannaJeon <jks7psj@gmail.com>
2018-01-23 18:31:53 +0900

    테스트코드 만들었더요 헿

--- src/test/java/bowling/model/ScoreTest.java
+++ src/test/java/bowling/model/ScoreTest.java
@@ -5,6 +5,9 @@ import bowling.model.score.Score;
 import bowling.model.score.SpareScore;
 import bowling.model.score.StrikeScore;
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class ScoreTest {
@@ -17,8 +20,7 @@ public class ScoreTest {
 
 	@Test
 	public void 미스_현재점수_보기() {
-		Score score = new MissScore(8, 1);
-		assertThat(score.toString()).isEqualTo("8 | 1");
+		assertThat(Score.convertString(Arrays.asList(8, 1))).isEqualTo("8|1");
 	}
 
 	@Test
@@ -29,8 +31,7 @@ public class ScoreTest {
 
 	@Test
 	public void 스페어_현재점수_보기() {
-		Score score = new SpareScore(8);
-		assertThat(score.toString()).isEqualTo("8 | /");
+		assertThat(Score.convertString(Arrays.asList(8, 2))).isEqualTo("8|/");
 	}
 
 	@Test
@@ -41,19 +42,16 @@ public class ScoreTest {
 
 	@Test
 	public void 스트라이크_현재점수_보기() {
-		Score score = new StrikeScore();
-		assertThat(score.toString()).isEqualTo("X");
+		assertThat(Score.convertString(Arrays.asList(10))).isEqualTo("X");
 	}
 
 	@Test
 	public void 미스_거터처리() {
-		Score score = new MissScore(3, 0);
-		assertThat(score.toString()).isEqualTo("3 | -");
+		assertThat(Score.convertString(Arrays.asList(3, 0))).isEqualTo("3|-");
 	}
 
 	@Test
 	public void 스페어_거터처리() {
-		Score score = new SpareScore(0);
-		assertThat(score.toString()).isEqualTo("- | /");
+		assertThat(Score.convertString(Arrays.asList(0, 10))).isEqualTo("-|/");
 	}
 }
