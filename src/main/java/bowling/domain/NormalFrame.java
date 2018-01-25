4e935d426da4704975a184e1a60209d164c14045
rogue <retrokarr@gmail.com>
2018-01-25 23:36:20 +0900

    feat : create NormalFrame class

--- src/main/java/bowling/domain/NormalFrame.java
+++ src/main/java/bowling/domain/NormalFrame.java
@@ -1,5 +1,7 @@
 package bowling.domain;
 
+import bowling.util.ScoreCalculator;
+
 public class NormalFrame extends Frame {
     private Frame nextFrame;
 
@@ -8,5 +10,32 @@ public class NormalFrame extends Frame {
             throw new IllegalArgumentException("Next frame can't be a null");
 
         this.nextFrame = nextFrame;
+    }
+
+    @Override
+    public Integer getFrameScore() {
+        if(score != null)
+            return score;
+
+        if(isStrike())
+            return score = nextFrame.calculateStrike();
+
+        if(isSpare())
+            return score = nextFrame.calculateSpare();
+
+        return score = ScoreCalculator.calculateFrame(firstTry, secondTry);
+    }
+
+    @Override
+    public Integer calculateStrike() {
+        if(isStrike())
+            return ScoreCalculator.calculateDoubleStrike(nextFrame.calculateSpare());
+
+        return ScoreCalculator.calculateStrike(firstTry, secondTry);
+    }
+
+    @Override
+    public Integer calculateSpare() {
+        return ScoreCalculator.calculateSpare(firstTry);
     }
 }
--- src/test/java/bowling/domain/NormalFrameTest.java
+++ src/test/java/bowling/domain/NormalFrameTest.java
@@ -3,16 +3,51 @@ package bowling.domain;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class NormalFrameTest {
     private NormalFrame normalFrame;
 
     @Before
     public void init() {
-        normalFrame = new NormalFrame(new FinalFrame());
+        FinalFrame finalFrame = new FinalFrame();
+        finalFrame.rollBowlingBall(new Pin(5));
+        finalFrame.rollBowlingBall(new Pin(5));
+
+        normalFrame = new NormalFrame(finalFrame);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void invalidInputTest() {
         new NormalFrame(null);
+    }
+
+    @Test
+    public void calculateSpareTest() {
+        normalFrame.rollBowlingBall(new Pin(4));
+        assertThat(normalFrame.calculateSpare()).isEqualTo(14);
+    }
+
+    @Test
+    public void calculateStrikeTest() {
+        normalFrame.rollBowlingBall(new Pin(10));
+        assertThat(normalFrame.calculateStrike()).isEqualTo(25);
+    }
+
+    @Test
+    public void calculateStrikeTest2() {
+        normalFrame.rollBowlingBall(new Pin(5));
+        assertThat(normalFrame.calculateStrike()).isEqualTo(null);
+
+        normalFrame.rollBowlingBall(new Pin(5));
+        assertThat(normalFrame.calculateStrike()).isEqualTo(20);
+    }
+
+    @Test
+    public void getFrameScore() {
+        assertThat(normalFrame.getFrameScore()).isEqualTo(null);
+        normalFrame.rollBowlingBall(new Pin(5));
+        normalFrame.rollBowlingBall(new Pin(3));
+        assertThat(normalFrame.getFrameScore()).isEqualTo(8);
     }
 }
