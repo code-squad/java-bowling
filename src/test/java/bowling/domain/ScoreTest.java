f267be5240207e67c88decf6d3c60268dc01c0e1
rogue <retrokarr@gmail.com>
2018-01-27 00:15:34 +0900

    feat : test code creation

--- src/test/java/bowling/domain/ScoreTest.java
+++ src/test/java/bowling/domain/ScoreTest.java
@@ -1,4 +1,37 @@
 package bowling.domain;
 
+import bowling.exception.CannotCalculateException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class ScoreTest {
+    private Score score;
+
+    @Before
+    public void init() {
+        score = new Score(5, 1);
+    }
+
+    @Test
+    public void bowlTest() {
+        assertThat(score.bowl(5)).isEqualTo(new Score(10, 0));
+    }
+
+    @Test(expected = CannotCalculateException.class)
+    public void getScoreTest() {
+        assertThat(score.getScore()).isEqualTo(5);
+    }
+
+    @Test
+    public void getScoreTest2() {
+        score = score.bowl(2);
+        assertThat(score.getScore()).isEqualTo(10);
+    }
+
+    @Test
+    public void calculableTest() {
+        assertThat(score.calculable()).isFalse();
+    }
 }
