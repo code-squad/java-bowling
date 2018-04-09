5fac97b0402f350efec7dedbe652d29e7bfbc3f6
krapeaj <jaeepark@gmail.com>
2018-04-09 17:15:25 +0900

    Modify functions in NormalFrame so that a toString call would return an entire score string

--- src/main/java/domain/NormalFrame.java
+++ src/main/java/domain/NormalFrame.java
@@ -1,23 +1,43 @@
 package domain;
 
 public class NormalFrame {
-    private final int firstThrow;
-    private final int secondThrow;
+    private static final int MAX = 10;
+    private final int score;
+    private final boolean strike;
+    private final boolean spare;
 
-    public NormalFrame(int firstThrow, int secondThrow) {
-        this.firstThrow = firstThrow;
-        this.secondThrow = secondThrow;
+    private NormalFrame(int score, boolean strike, boolean spare) {
+        this.score = score;
+        this.strike = strike;
+        this.spare = spare;
     }
 
-    public boolean isStrike() {
-        return firstThrow == 10;
+    public NormalFrame ofNormalFrame(int firstThrow, int secondThrow) {
+        if (isStrike(firstThrow)) {
+            return new NormalFrame(10, true, false);
+        }
+        if (isSpare(firstThrow, secondThrow)) {
+            return new NormalFrame(10, false, true);
+        }
+        return new NormalFrame(firstThrow + secondThrow, false, false);
     }
 
-    public boolean isSpare() {
-        return firstThrow + secondThrow == 10;
+    public static boolean isStrike(int firstThrow) {
+        return firstThrow == MAX;
     }
 
-    public boolean isNormal() {
-        return firstThrow + secondThrow < 10;
+    public static boolean isSpare(int firstThrow, int secondThrow) {
+        return firstThrow + secondThrow == MAX;
+    }
+
+    @Override
+    public String toString() {
+        if(strike){
+            return ScoreMarker.markStrike();
+        }
+        if(spare){
+            return ScoreMarker.markSpare();
+        }
+        return ScoreMarker.markNumber();
     }
 }
