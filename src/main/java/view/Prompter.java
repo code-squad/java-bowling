ee662dd4a8d6360c58f983f5222ed43025f169c7
krapeaj <jaeepark@gmail.com>
2018-04-11 15:01:51 +0900

    Create user prompt

---
+++ src/main/java/view/Prompter.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.Scanner;
+
+public class Prompter {
+
+    private static String takeInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static String promptForName() {
+        System.out.print("플레이어 이름은(3 english letters)?: ");
+        return takeInput();
+    }
+
+    public static String promptForThrow(int frameNumber) {
+        System.out.printf("%d 프레임 투구: ", frameNumber);
+        return takeInput();
+    }
+}
