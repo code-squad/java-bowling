b056a042a880ae5b1f65af7ef01b88a8c14dc317
durin93 <lsc401@naver.com>
2018-04-17 01:40:43 +0900

    step2

--- src/main/java/bowling/domain/NormalFrame.java
+++ src/main/java/bowling/domain/NormalFrame.java
@@ -2,19 +2,24 @@ package bowling.domain;
 
 public class NormalFrame extends Frame {
 
-	public NormalFrame(int frameNum, Score score) {
-		super(frameNum, score.getScore(0), score.getScore(1));
+	public NormalFrame(Pin pin, Score score) {
+		super(pin.getPin(0), pin.getPin(1), score);
 	}
 
-	public NormalFrame(int frameNum, int firstDownPin) {
-		super(frameNum, firstDownPin);
+	public NormalFrame(int firstDownPin, Score score) {
+		super(firstDownPin, score);
 	}
 
-	public static NormalFrame of(int frameNum, Score score) {
-		if (score.size() == 1) {
-			return new NormalFrame(frameNum, score.getScore(0));
+	public static NormalFrame of(Pin pin, int beforeScore) {
+		if (pin.isStrike()) {
+			return new NormalFrame(pin.getPin(0), Score.ofStrike(pin.beforePlusNow(beforeScore)));
 		}
-		return new NormalFrame(frameNum, score);
+
+		if (pin.isSpare()) {
+			return new NormalFrame(pin, Score.ofSpare(pin.beforePlusNow(beforeScore)));
+		}
+
+		return new NormalFrame(pin, Score.of(pin.beforePlusNow(beforeScore)));
 	}
 
 	public String getDownPin() {
