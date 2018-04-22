71d4a9e305a11137bbb70bc2946f33e312cb2ff8
JaeyeonKIM <foodsksms@gmail.com>
2018-04-22 23:00:52 +0900

    LastFrame 만드는중

--- src/main/java/frame/LastFrame.java
+++ src/main/java/frame/LastFrame.java
@@ -1,7 +1,44 @@
 package frame;
 
 import game.Pin;
+import status.*;
 
 public class LastFrame {
+    private Pin firstPin;
+    private Pin secondPin;
+    private Pin thridPin;
+
+    public Status shoot(int pinFall) {
+        if (firstPin == null) {
+            firstPin = new Pin(pinFall);
+            return new Default(firstPin);
+        }
+        secondPin = new Pin(pinFall);
+
+        if (firstPin.getPinFall() == 10) {
+            thridPin = new Pin(pinFall);
+        }
+
+        if (firstPin.getPinFall() != 10 && (firstPin.getPinFall() + secondPin.getPinFall() == 10)) {
+            thridPin = new Pin(pinFall);
+        }
+        return checkStatus();
+    }
+
+    public Status checkStatus() {
+        if (firstPin.getPinFall() != Pin.getMaxFall() && secondPin == null)
+            return new Default(firstPin);
+
+        if (firstPin.getPinFall() == Pin.getMaxFall() && secondPin == null)
+            return new Strike(firstPin);
+
+        if ((firstPin.getPinFall() + secondPin.getPinFall() == 10) && firstPin.getPinFall() != Pin.getMaxFall())
+            return new Spare(firstPin, secondPin);
+
+        if (secondPin.getPinFall() == 0)
+            return new Missing(firstPin, secondPin);
+
+        return new Normal(firstPin, secondPin);
+    }
 
 }
