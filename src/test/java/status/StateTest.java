94f34bbc227b3d0fe0de2ce1776a7c1da2029b36
jimmy <foodsksms@gmail.com>
2018-04-24 15:16:55 +0900

    불필요 파일 삭제

--- src/test/java/status/StateTest.java
+++ src/test/java/status/StateTest.java
@@ -17,28 +17,8 @@ public class StateTest {
     }
 
     @Test
-    public void 스트라이크() {
-        State strike = frame.bowl(10);
-        assertEquals(strike instanceof Strike, true);
-    }
-
-    @Test
     public void ready테스트() {
         String result = frame.state.displayText();
         Assert.assertThat(null, is(result));
-    }
-
-    @Test
-    public void 스페어테스트() {
-        frame.bowl(8);
-        State spare = frame.state.bowl(2);
-        assertEquals(spare instanceof Spare, true);
-    }
-
-    @Test
-    public void 미싱테스트() {
-        frame.bowl(5);
-        State miss = frame.state.bowl(4);
-        assertEquals(miss instanceof Miss, true);
     }
 }
